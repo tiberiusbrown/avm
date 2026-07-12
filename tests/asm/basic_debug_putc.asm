2c7c30582f279edf30194bc17daa03abbe6963a3
Peter Brown <brow1067@gmail.com>
2026-07-12 14:00:35 -0600

    test infrastructure

--- tests/asm/basic_debug_putc.asm
+++ tests/asm/basic_debug_putc.asm
@@ -1,15 +1,12 @@
 .section .text,"ax",@progbits
 
-.equ SYS_DEBUG_PUTC, 0
-.equ SYS_DEBUG_BREAK, 1
-
 .globl _start
 .type _start, @function
 .p2align 1
 
 _start:
-    ldi8   c0, 'P'
-    sys    SYS_DEBUG_PUTC
-    sys    SYS_DEBUG_BREAK
+    ldi8 c0, 'P'
+    sys  0          ; debug_putc
+    sys  1          ; debug_break
 
 .size _start, .-_start

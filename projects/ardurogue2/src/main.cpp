e7ab58187959f3626df0d959c525e32e4ba48895
Peter Brown <brow1067@gmail.com>
2026-07-26 21:37:57 -0600

    add ardurogue2 project

--- projects/ardurogue2/src/main.cpp
+++ projects/ardurogue2/src/main.cpp
@@ -2,9 +2,9 @@ extern "C" int main()
 {
     while(true)
     {
-        //__avm_draw_filled_rect_white(10, 10, 20, 20);
+        __avm_draw_filled_rect_white(10, 10, 20, 20);
 
-        //__avm_display();
+        __avm_display(1);
     }
     return 0;
 }
